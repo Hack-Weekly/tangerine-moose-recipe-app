63d4a5b47160ca5077398115916fdd70f090ddc1
Shashank Bhat <shashank.bhat@virtualreviewassist.com>
2023-06-30 22:41:38 -0400

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Search from "./pages/Search";
+import Navbar from "./components/utility/Navbar";
+import MyRecipes from "./pages/MyRecipes";
+import AddRecipe from "./pages/AddRecipe";
+import Recipe from "./pages/Recipe";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Navbar />}>
+          <Route path='search' element={<Search />} />
+          <Route path='my-recipes' element={<MyRecipes />} />
+          <Route path='add' element={<AddRecipe />} />
+          <Route path='recipe/:id' element={<Recipe />} />
+        </Route>
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </>
   );
 }
 
