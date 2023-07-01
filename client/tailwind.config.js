8a4586f0c0adb728c49d6a00428fa7b203eef17c
Shashank Bhat <shashank.bhat@virtualreviewassist.com>
2023-06-30 22:44:26 -0400

    modify tailwind config and package.json

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".src/**/*.{js, jsx}"],
+  content: ["./src/**/*.{js, jsx}", "./src/components/**/*.{jsx, js}", "./src/pages/**/*.{jsx, js}"],
   theme: {
     extend: {},
   },
