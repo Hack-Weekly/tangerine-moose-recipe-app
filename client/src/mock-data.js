64e1a24ac3dd04ac9776a898f1b172a3e64779d5
Shashank Bhat <shashank.bhat@virtualreviewassist.com>
2023-06-30 22:45:19 -0400

    Add mock data to test

---
+++ client/src/mock-data.js
@@ -0,0 +1,90 @@
+export const data = [
+  {
+    id: 1,
+    title: "Biryani Badshahi",
+    ingredients:
+      "1 kg Mutton|500 g Rice parboiled|3 tb Lemon juice|20 Blanched almonds|1 tb Mint leaves; (chopped)|2 c Butter|2 Handfuls chopped coriander leaves|1 tb Cumin seeds|4 lg Sliced onions|4 Whole cardamoms|2 tb Sweet oil|8 Pods garlic|4 Whole cloves|1 2 inch long ginger|1 tb Saffron|1 tb Green chillies chopped|1 tb Red chillies powdered|1 1 inch piece cinnamon|1 kg Curd|250 g Milk|6 c Water|Salt to taste.",
+    servings: "1 servings",
+    instructions:
+      "First, wash and soak rice. Then fry sliced onions to a golden brown colour. Soak saffron in water. Now grind ginger, red chillies, garlic and almonds and fry these in butter. Add to it mutton and salt and stir for five minutes. Now add water and cook on a slow fire till meat becomes tender and about 2 cups of the gravy is left. Boil rice again with salt in another pan. Put curd into a piece of muslin cloth and let the water drain away. Add ckives, cardamom, cumin seeds, mint leaves, chopped chillies and coriander in drained curd. Strain the saffron water and add lemon juice. Add all this to mutton. Sprinkle half of the boiled rice over the mutton, then spread a layer of fried onion and then of rice again. Now pour milk and some butter and cover the vessel. Seal edges of the pan with flour paste. Place can on a fire for one hour. Serve it very hot with some curry.",
+  },
+  {
+    id: 2,
+    title: "Vegetable and Bean Biryani",
+    ingredients:
+      "1 md Aubergine; cubed (large is better, though)|1 pn Salt and black pepper|4 tb Vegetable oil or vegetable ghee|3/4 ts Poppy seeds|1 ts Mustard seeds|1 pn Cayenne|1/4 ts Turmeric|3/4 ts Garam masala|1/4 ts Ground coriander|1 Red pepper; seeded & sliced|3 oz Butter beans; cooked|2 Tomatoes; skinned, seeded & chopped|8 oz Long grained brown rice; cooked with saffron|1 oz Toasted pine nuts|1 oz Raisins|1 Coriander sprig for garnish*",
+    servings: "4 Servings",
+    instructions:
+      "Place the aubergine cubes in a colander, sprinkle with salt and leave for 30 minutes. Rinse the cubes and dry. Heat the oil or ghee in a large pan. Add the poppy and mustard seeds and cook for 2 minutes, stirring. Add the cayenne pepper, turmeric, garam masala, coriander, aubergine, red pepper, beans, tomatoes and salt and pepper. Cover and cook for about 10 minutes. Layer the rice and vegetable mixture in an ovenproof dish. Cover and cook in a preheated moderate oven (180~C, 350~F, Gas Mark 4) for 30 minutes. To serve: Sprinkle with toasted pine nuts and raisins. Garnish with a coriander sprig and serve hot. * I've never used sprig of coriander - can't stand it unless it's ground coriander, from a packet! It's called cilantro in the US, isn't it?",
+  },
+  {
+    id: 3,
+    title: "Kohnen's Biryani Supremo",
+    ingredients:
+      "1 tb Oil|2/3 c Long Grain Rice (Basmati best)|4 Cloves|2 Piece Cinnamon|1 ts Ground Cumin|1 ts Ground Turmeric|1 ts Fresh Ground Black Pepper|1 1/4 c Chicken Broth (1 can)|1 Salam Leaf or Bay Leaf|1/3 c Golden Raisins, Raisins or Both|About as much Cooked Chicken as you will get off two Thighs|2 Hard Boiled Eggs|2 Or 3 Tomatoes|Fried Onion Flakes",
+    servings: "1 Servings",
+    instructions:
+      "Here's another, similar, recipe that's a bit more elaborate. Made it once and it was quite good. I usually make the other as it's more simple. The Nasi Kuning is more of a side dish while this makes a pretty satisfying meal. Heat the oil in a 1 1/2-quart saucepan over medium-high heat until hot. Add the rice and stir until it begins to brown, now add the cloves, cinnamon, turmeric and pepper; stir for a minute or so. Pour in the broth, add the salam or bay leaf and cumin. Bring to a rapid boil then cover and reduce the heat to a very low simmer. Simmer for about 40 minutes. Stir in the chicken and raisins; cover again and let sit, off the stove, for a few minutes before serving. Garnish with egg and tomato slices and the Onion Flakes. This is the basic Recipe. For variety try a teaspoon of ground chili instead of the black pepper. Other meats could be substituted for the chicken. Indonesian Crisp Beef is quite good with this, using it as a garnish instead of mixing in with the rice. Try anything you like, but don't put anything in to cook along with the rice, it might upset the liquid ratio and spoil things. The rice should come out with the grains separate and no stickiness.",
+  },
+  {
+    id: 4,
+    title: "Chicken Biryani",
+    ingredients:
+      "1/2 c Vegetable oil|3 Medium onions, chopped fine|2 Cloves of Garlic, chopped|1 oz Fresh ginger, chopped|3/4 lb Boneless chicken|2 Brown Cardamon pods|4 Whole Cloves|14 Whole Black Peppercorns|2 ts Dried Coriander|1 ts Cumin seeds|2 ts White poppy seeds|2 ts Fresh lemon juice|1/2 ts Garam Marsala|1/4 ts Cayenne Pepper|1/2 ts Salt|2 ts Tomato Paste|2 Bay Leaves|3/4 c Plain yogurt|1 Saffron rice recipe|1 Biryani recipe|1 Tomato slices|1 Green pepper rings|1 Fresh coriander",
+    servings: "6 Servings",
+    instructions:
+      "Heat oil in large, heavy frying pan and saute onions and garlic until lightly browned. Add ginger, fry another minute or two, then transfer mixture to a large bowl. Add chicken. Grind together the cardamom, cloves, peppercorns, dried coriander, cumin and poppy seeds. Blend with lemon juice, garam marsala, cayenne, salt, tomato paste, by leaves and yogurt. Stir mixture into vegetable and chicken mixture, cover and refrigerate for several hours. Using a large, heavy frying pan, cook mixture, covered, over low heat for 10 - 15 minutes, stirring often. From The Gazette 91/01/30.",
+  },
+  {
+    id: 5,
+    title: "South African Vegetable Biryani",
+    ingredients:
+      '3 lg Onions, sliced|4 tb Ghee|6 Chili peppers, crushed into - a paste -=OR=-|2 ts Cayenne|1 2" piece ginger|10 Garlic cloves|1/2 c Lentils, dried|1/2 lb Green peas, shelled|1/2 lb Carrots, chopped|1/2 lb Green beans, chopped|3 lg Tomatoes, chopped|6 Whole cloves|1 4" piece cinnamon stick|6 Cardamom pods, crushed|1 ts Turmeric|3 Sprigs mint, pounded -=OR=-|1/2 ts Dried mint|2 c Long grain white rice|6 lg Potatoes, chopped|2 ts Salt',
+    servings: "6 Servings",
+    instructions:
+      "In a large, heavy skillet, fry the onions in the ghee until golden. With a slotted spoon, remove 1/3 of the slices & set aside. Add to the pot, the ginger, garlic & chili paste. Fry for 5 or 6 minutes, stirring constantly. Add the lentils, peas, carrots & beans. Reduce heat & cook for 15 minutes. Add more ghee if necessary. Add tomatoes, spices & mint Stir for 5 minutes. Pour in 1 c of very hot water, cover & simmer until the vegetables are half-cooked. Add the rice, potatoes, salt & another 4 to 5 c of hot water. Cover & cook for 20 minutes or until the rice is done & the water is absorbed. Garnish with the reserved onion slices.",
+  },
+  {
+    id: 6,
+    title: "Biryani",
+    ingredients: "6 tb Butter|1/2 c Almonds|1/2 c Cashews|1/2 c Raisins|3/4 c Chicken stock|Saffron Rice",
+    servings: "6 Servings",
+    instructions:
+      "Heat 4 tablespoons of butter in a heavy frying pan and saute almonds, cashews and raisins for about 2 minutes. Stir nut mixture into Saffron Rice. TO SERVE: Spread 1/3 of saffron rice mixture evenly in ovenproof dish. Spread 1/2 meat mixture over rice. Repeat once again and finish with final 1/3 of rice. Half an hour before serving, heat chicken stock, add remaining butter to stock and pour over rice and meat dish. cover and bake in oven preheated to 350 degrees for 20 ~ 25 minutes or until all liquid is absorbed. Serve trimmed with tomato slices, green pepper rings and fresh coriander. From The Gazette, 91/01/30.",
+  },
+  {
+    id: 7,
+    title: "Biryani Rice",
+    ingredients: "3/4 c Basmati or Long Grain Rice|1 1/2 c Less 2 tb Water|1 ts Oil|Salt To Taste",
+    servings: "4 Servings",
+    instructions:
+      "Wash rice and put in a small saucepan with all above ingredients. Bring it to a boil on high heat, then cover and simmer gently until all the water is absorbed. The grains should be tender but firm. Set aside uncovered.",
+  },
+  {
+    id: 8,
+    title: "Biryani Rice Recipe",
+    ingredients:
+      "3/4 c Rice Basmati or long-grain rice|1 1/2 c Less 2 tbsp water|1 ts Oil|Salt to taste|1/2 lb Chicken cut into small pieces|3 tb Oil|1 lg Onion chopped|1/2 ts Ginger paste|1/2 ts Garlic paste|1 ts Ground cumin|1 ts Ground coriander|1/2 ts Turmeric powder|1 Bay leaf|2 Cloves|2 Cardamoms|pn Cinnamon|1 sm Tomato quartered|1 sm Potato peeled and quartered|3 tb Plain yogurt|1/4 ts Saffron",
+    servings: "8 Servings",
+    instructions:
+      "Wash rice and put in a small saucepan with all above ingredients. Bring it to a boil on high heat, then cover and simmer gently until all the water is absorbed. The grains should be tender but firm. Set aside uncovered. Heat oil in a large saucepan (medium heat), add onions and fry till golden around the edges, add garlic and ginger. All at once add all the spices and fry one minute longer. Add all other ingredients except saffron. Add salt to taste. Cover and cook on low heat till chicken is tender, stirring occasionally (about 1/2 hour). There should be very little liquid remaining in the pan. (During cooking a little water may be added if necessary to avoid sticking). Add half the prepared rice, spreading it evenly over the mixture, drizzle one tbsp of oil over it and add the saffron mixed into two tsp of milk. Cover with remaining rice, cover and cook on very low heat for ten minutes. Serve accompanied with whipped, lightly sweetened yogurt. The chicken could be substituted with 1/2 pound of mixed vegetables.",
+  },
+  {
+    id: 9,
+    title: "Vegetable Biryani",
+    ingredients:
+      "1 1/2 c Basmati Rice|1 tb Oil -- or ghee|1 lg Pinch Saffron-optional|1 pn Turmeric|1/2 ts Salt|2 1/4 c Hot Water Onion Mixture-----|1 c Chopped Onion|2 tb Oil -- or ghee|1 ts Fresh Ginger Root -- peeled|& grated|1 1/2 ts Cumin|1 1/2 ts Coriander|1/2 ts Cinnamon|1 pn Cayenne Vegetables-----|1/2 c Water|1 Carrot -- diced|2 c Cauliflower -- florets|(small)|1 Green Pepper -- diced|1 Tomato -- diced|1/2 c Peas|1/3 c Raisins|3/4 c Chickpeas|1/3 c Cashew Pieces -- unsalted|Salt -- to taste",
+    servings: "1 Servings",
+    instructions:
+      "In a saucepan, saute the rice briefly in one tablespoon oil, stirring to coat each kernel. Crumble in the saffron. Add the turmeric, salt, and the hot water. Bring the rice to a boil, cover the pan, and reduce the heat. Simmer for 20 minutes. While the rice is cooking, saute the onions in the oil for 5 minutes. Mix in the ginger, cumin, coriander, cinnamon, and cayenne. Add 1/2 cup water, the carrot and cauliflower. Cover the pan and cook on low heat for 3-4 minutes. Stir in the pepper, tomato, peas, raisins, chick peas, and cashews. Continue to simmer until the vegetables are barely tender. Add salt to taste. Mix vegetables with cooked riced. Place all in a 2-quart casserole, cover, and bake at 350 for 30 minutes. :",
+  },
+  {
+    id: 10,
+    title: "Vegetable Biryani (Ff)",
+    ingredients:
+      "Basmati Rice Vegetables Cumin Turmeric Cayenne Pepper Garam Masala Potatoes Cinnamon sticks Black Cardamoms Garlic Curry Leaves Saffron Skim Milk",
+    servings: "1 Servings",
+    instructions:
+      "Parboil good rice (not the supermarket junk rice, get a nice basmati -- most indians seem to favor elephant brand). water sautee your favorite veges, but dont make them soft. add roasted cumin, very little turmeric, cayenne pepper and garam masala (i usually mixes the spices and dry roated together before adding to veges). in a large pan with a lid layer rice/vege/rice (and repeat as needed). Stick a in few potatoes, a few sticks of cinnamon, a few black cardamoms, and many cloves of garlic, and some curry leaves. On top pour in saffron dissolved in skim milk. Add about 1/4 to 1/2 cup water. Secure and seal the lid (i seal by sticking dough right around the lid). bake in oven at medium (350 or so) for about an hour (just when you are ready to eat take it out). Its great and FF!! (i like to bake it enough to get some burnt rice at the bottom of the pan. you may want to spray the pan lightly with PAM to make it easier to get the burnt rice out)",
+  },
+];
